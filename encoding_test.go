645f93e130ed204fffde9b6a42c26e55c3f9d106
Omer Ben-Hayun <Omer.Ben-Hayun@ibm.com>
2021-03-09 17:06:39 +0200

    first commit [WIP-delete this comment]

--- encoding_test.go
+++ encoding_test.go
@@ -2,11 +2,40 @@ package libovsdb
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+// newOvsSetOrPanic is a convenience wrapper over NewOvsSet
+func newOvsSetOrPanic(s interface{})OvsSet{
+	// TODO add better panic explanations
+
+	//make sure that the input is a slice
+	if reflect.TypeOf(s).Kind() != reflect.Slice{
+		panic(s)
+	}
+	res, err := NewOvsSet(s)
+	if err !=nil{
+		panic(s)
+	}
+	return *res
+}
+
+// newOvsMapOrPanic is a convenience wrapper over NewOvsMap
+func newOvsMapOrPanic(v interface{})OvsMap{
+	// TODO add better panic explanations
+	//make sure that the input is a map
+	if reflect.TypeOf(v).Kind() != reflect.Map{
+		panic(v)
+	}
+	res, err := NewOvsMap(v)
+	if err !=nil{
+		panic(v)
+	}
+	return *res
+}
 
 // empty Set test
 func TestEmptySet(t *testing.T) {
@@ -47,3 +76,26 @@ func TestMap(t *testing.T) {
 	expected := "[\"map\",[[\"one\",\"first\"],[\"two\",\"second\"]]]"
 	assert.JSONEqf(t, expected, string(jsonStr), "they should be equal\n")
 }
+/*
+type tuple struct {
+	obj interface{}
+	json string
+}
+var flagtests = []tuple {
+	{"%a", "[%a]"},
+	{"%-a", "[%-a]"},
+	{"%+a", "[%+a]"},
+	{"%#a", "[%#a]"},
+	{"% a", "[% a]"},
+	{"%0a", "[%0a]"},
+	{"%1.2a", "[%1.2a]"},
+	{"%-1.2a", "[%-1.2a]"},
+	{"%+1.2a", "[%+1.2a]"},
+	{"%-+1.2a", "[%+-1.2a]"},
+	{"%-+1.2abc", "[%+-1.2a]bc"},
+	{"%-1.2abc", "[%-1.2a]bc"},
+}
+
+ */
+
+//func parserTest(t *testing.T)
